fa867cee5ee1d51e29414e514606abf8fba00588
Tom Wiesing <tkw01536@gmail.com>
2026-03-03 15:52:58 +0100

    LoginIdpLinkConfirm: add styles and message

--- src/login/pages/LoginIdpLinkConfirm.tsx
+++ src/login/pages/LoginIdpLinkConfirm.tsx
@@ -2,6 +2,8 @@ import type { PageProps } from "keycloakify/login/pages/PageProps";
 import type { KcContext } from "../KcContext";
 import type { I18n } from "../i18n";
 import CdiTemplate from "../components/CdiTemplate";
+import MessageAlert from "../components/MessageAlert";
+import styles from "./LoginIdpLinkConfirm.module.css";
 
 type LoginIdpLinkConfirmKcContext = Extract<KcContext, { pageId: "login-idp-link-confirm.ftl" }>;
 type LoginIdpLinkConfirmProps = Omit<PageProps<LoginIdpLinkConfirmKcContext, I18n>, "Template">;
@@ -9,23 +11,21 @@ type LoginIdpLinkConfirmProps = Omit<PageProps<LoginIdpLinkConfirmKcContext, I18
 export default function LoginIdpLinkConfirm(props: LoginIdpLinkConfirmProps) {
     const { kcContext, i18n } = props;
 
-    const { url, idpAlias } = kcContext;
+    const { url, idpAlias, message } = kcContext;
     const { msg } = i18n;
+
+    const messageNode = message ? <MessageAlert type={message.type} summary={message.summary} /> : null;
 
     return (
         <CdiTemplate kcContext={kcContext} i18n={i18n} doUseDefaultCss={false} headerNode={msg("confirmLinkIdpTitle")}>
-            <form action={url.loginAction} method="post">
-                <div>
-                    <button type="submit" name="submitAction" value="updateProfile" data-second-button>
-                        {msg("confirmLinkIdpReviewProfile")}
-                    </button>
-                </div>
-                &nbsp;
-                <div>
-                    <button type="submit" name="submitAction" value="linkAccount" data-action-button>
-                        {msg("confirmLinkIdpContinue", idpAlias)}
-                    </button>
-                </div>
+            <form action={url.loginAction} method="post" className={styles.form}>
+                {messageNode}
+                <button type="submit" name="submitAction" value="linkAccount" data-action-button>
+                    {msg("confirmLinkIdpContinue", idpAlias)}
+                </button>
+                <button type="submit" name="submitAction" value="updateProfile" data-second-button>
+                    {msg("confirmLinkIdpReviewProfile")}
+                </button>
             </form>
         </CdiTemplate>
     );
