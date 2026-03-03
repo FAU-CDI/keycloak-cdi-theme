84c351767baa0e0c1905501db97cbcb001b3cfc2
Tom Wiesing <tkw01536@gmail.com>
2026-03-03 15:55:53 +0100

    LoginIdpLinkEmail: add message

--- src/login/pages/LoginIdpLinkEmail.tsx
+++ src/login/pages/LoginIdpLinkEmail.tsx
@@ -2,6 +2,7 @@ import type { PageProps } from "keycloakify/login/pages/PageProps";
 import type { KcContext } from "../KcContext";
 import type { I18n } from "../i18n";
 import CdiTemplate from "../components/CdiTemplate";
+import MessageAlert from "../components/MessageAlert";
 
 type LoginIdpLinkEmailKcContext = Extract<KcContext, { pageId: "login-idp-link-email.ftl" }>;
 type LoginIdpLinkEmailProps = Omit<PageProps<LoginIdpLinkEmailKcContext, I18n>, "Template">;
@@ -9,11 +10,14 @@ type LoginIdpLinkEmailProps = Omit<PageProps<LoginIdpLinkEmailKcContext, I18n>, 
 export default function LoginIdpLinkEmail(props: LoginIdpLinkEmailProps) {
     const { kcContext, i18n } = props;
 
-    const { url, realm, brokerContext, idpAlias } = kcContext;
+    const { url, realm, brokerContext, idpAlias, message } = kcContext;
     const { msg } = i18n;
+
+    const messageNode = message ? <MessageAlert type={message.type} summary={message.summary} /> : null;
 
     return (
         <CdiTemplate kcContext={kcContext} i18n={i18n} doUseDefaultCss={false} headerNode={msg("emailLinkIdpTitle", idpAlias)}>
+            {messageNode}
             <p>{msg("emailLinkIdp1", idpAlias, brokerContext.username, realm.displayName)}</p>
             <p>
                 {msg("emailLinkIdp2")} <a href={url.loginAction}>{msg("doClickHere")}</a> {msg("emailLinkIdp3")}
