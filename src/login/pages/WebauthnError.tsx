e7d0369916c9f38ce61f7223b32f5dadf962c2a7
Tom Wiesing <tkw01536@gmail.com>
2026-04-07 10:42:45 +0200

    WebAuthn: Fix WebAuthn error page and add i18n support

--- src/login/pages/WebauthnError.tsx
+++ src/login/pages/WebauthnError.tsx
@@ -2,26 +2,25 @@ import type { PageProps } from "keycloakify/login/pages/PageProps";
 import type { KcContext } from "../KcContext";
 import type { I18n } from "../i18n";
 import CdiTemplate from "../components/CdiTemplate";
-import MessageAlert from "../components/MessageAlert";
 import { CDIButton } from "../components/CDIButton";
+import { useRef } from "react";
 
 export default function WebauthnError(props: PageProps<Extract<KcContext, { pageId: "webauthn-error.ftl" }>, I18n>) {
     const { kcContext, i18n } = props;
 
-    const { url, isAppInitiatedAction, message } = kcContext;
+    const { url, isAppInitiatedAction } = kcContext;
 
     const { msg, msgStr } = i18n;
 
-    const showMessage = message !== undefined && (message.type !== "warning" || !isAppInitiatedAction);
-    const messageNode = showMessage && message ? <MessageAlert type={message.type} summary={message.summary} /> : null;
+    const isSetRetry = useRef<HTMLInputElement>(null);
+    const executionValue = useRef<HTMLInputElement>(null);
+    const kcErrorCredentialForm = useRef<HTMLFormElement>(null);
 
     return (
-        <CdiTemplate kcContext={kcContext} i18n={i18n} doUseDefaultCss={false} displayMessage={true} headerNode={msg("webauthn-error-title")}>
-            {messageNode}
-
-            <form id="kc-error-credential-form" action={url.loginAction} method="post">
-                <input type="hidden" id="executionValue" name="authenticationExecution" />
-                <input type="hidden" id="isSetRetry" name="isSetRetry" />
+        <CdiTemplate kcContext={kcContext} i18n={i18n} doUseDefaultCss={false} headerNode={msg("webauthn-error-title")}>
+            <form ref={kcErrorCredentialForm} action={url.loginAction} method="post">
+                <input type="hidden" ref={executionValue} name="authenticationExecution" />
+                <input type="hidden" ref={isSetRetry} name="isSetRetry" />
             </form>
 
             <div>
@@ -31,16 +30,16 @@ export default function WebauthnError(props: PageProps<Extract<KcContext, { page
                     name="try-again"
                     id="kc-try-again"
                     onClick={() => {
-                        (document.getElementById("isSetRetry") as HTMLInputElement | null)?.setAttribute("value", "retry");
-                        (document.getElementById("executionValue") as HTMLInputElement | null)?.setAttribute("value", "${execution}");
-                        (document.getElementById("kc-error-credential-form") as HTMLFormElement | null)?.requestSubmit();
+                        isSetRetry.current?.setAttribute("value", "retry");
+                        executionValue.current?.setAttribute("value", "${execution}");
+                        kcErrorCredentialForm.current?.requestSubmit();
                     }}
                 >
                     {msgStr("doTryAgain")}
                 </CDIButton>
             </div>
             {isAppInitiatedAction && (
-                <form action={url.loginAction} id="kc-webauthn-settings-form" method="post">
+                <form action={url.loginAction} method="post">
                     <CDIButton secondary type="submit" id="cancelWebAuthnAIA" name="cancel-aia" value="true">
                         {msgStr("doCancel")}
                     </CDIButton>
